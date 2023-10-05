7cac05b48498188d967757ca8c1346c8bdb6e760
sama5677 <samaamohamed687@gmail.com>
2023-10-05 02:34:44 -0700

    Build my own calculator!

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,19 +1,10 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
 
+if __name__ == "__main__":
+    """Handle basic arithmetic operations."""
+    from calculator_1 import add, sub, mul, div
+    import sys
 
-def arg_calc(argv):
-    n = len(argv) - 1
-    if n != 3:
+    if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
-    a = int(argv[1])
-    op = argv[2]
-    b = int(argv[3])
-    if op == '+':
-        print("{:d} {:s} {:d} = {:d}".format(a, op, b, add(a, b)))
-    elif op == '-':
-        print("{:d} {:s} {:d} = {:d}".format(a, op, b, sub(a, b)))
-    elif op == '*':
-        print("{:d} {:s} {:d} = {:d}".format(a, op, b, mul(a, b)))
-    elif op == '/':
+        sys.exit(1)
